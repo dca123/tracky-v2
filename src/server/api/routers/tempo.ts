eca2d9353c9c5af2fb3989e30f1050a3f31bc2df
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-04-02 18:48:05 +0530

    test saving

--- src/server/api/routers/tempo.ts
+++ src/server/api/routers/tempo.ts
@@ -29,6 +29,8 @@ export const tempoRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ input }) => {
+      const myself = await getMyself();
+      const issues = await getIssues();
       const worklogs: Array<Worklog> = [];
       if (isMonday(input.startOfWeek) === false) {
         throw new Error("Start of week is not a Monday");
@@ -37,31 +39,46 @@ export const tempoRouter = createTRPCRouter({
         start: input.startOfWeek,
         end: addDays(input.startOfWeek, 4),
       });
-
-      for (const date of dates) {
-        for (const [day, logs] of Object.entries(input.logs)) {
-          const secondsPerEntry = hoursToSeconds(8) / logs.length;
-          for (const log of logs) {
-            let startTime = parse("08:00:00", "HH:mm:ss", new Date());
-            startTime = addSeconds(
-              startTime,
-              secondsPerEntry * logs.indexOf(log)
-            );
-
-            worklogs.push({
-              issueId: parseInt(log),
-              startDate: lightFormat(date, "yyyy-MM-dd"),
-              timeSpentSeconds: secondsPerEntry,
-              description: `Time entry for ${day}`,
-              account: "KPMG-FUNDS",
-              startTime: lightFormat(startTime, "HH:mm:ss"),
-            });
+      let dayIdx = 0;
+      for (const [day, logs] of Object.entries(input.logs)) {
+        const secondsPerEntry = hoursToSeconds(8) / logs.length;
+        for (const log of logs) {
+          let startTime = parse("08:00:00", "HH:mm:ss", new Date());
+          startTime = addSeconds(
+            startTime,
+            secondsPerEntry * logs.indexOf(log)
+          );
+          const date = dates[dayIdx];
+          if (date === undefined) {
+            throw new Error("Date is undefined");
           }
+          const issue = issues.find((issue) => issue.id === log);
+          if (issue === undefined) {
+            throw new Error("Issue not found");
+          }
+          worklogs.push({
+            issueId: parseInt(log),
+            startDate: lightFormat(date, "yyyy-MM-dd"),
+            timeSpentSeconds: secondsPerEntry,
+            description: `Did work on ${issue.title}`,
+            account: "KPMG-FUNDS",
+            startTime: lightFormat(startTime, "HH:mm:ss"),
+            authorAccountId: myself.accountId,
+          });
         }
+        dayIdx++;
       }
       const responses = [];
       for (const worklog of worklogs) {
-        const reponse = await tempoRequest("worklogs", "POST", worklog);
+        const reponse = await tempoRequest("worklogs", "POST", {
+          ...worklog,
+          attributes: [
+            {
+              key: "_Account_",
+              value: worklog.account,
+            },
+          ],
+        });
 
         responses.push(await reponse.json());
       }
@@ -76,17 +93,18 @@ type Worklog = {
   timeSpentSeconds: number;
   description: string;
   account?: "KPMG-FUNDS" | "INTERNAL";
+  authorAccountId: string;
 };
 
 export const tempoRequest = async <T>(
   api: "worklogs",
   method: "GET" | "POST" | "PUT" | "DELETE" = "GET",
-  body?: Record<string, T>
+  body: Record<string, T> & { accountId?: string }
 ) => {
   if (!process.env.TEMPO) {
     throw new Error("TEMP environment variable not set");
   }
-  if (process.env.PROD) {
+  if (true) {
     return fetch(`https://api.tempo.io/4/${api}`, {
       headers: {
         Authorization: `Bearer ${process.env.TEMPO}`,
@@ -111,4 +129,25 @@ export const tempoRequest = async <T>(
       });
     }
   );
+};
+
+const MyselfSchema = z.object({
+  accountId: z.string(),
+  emailAddress: z.string(),
+  displayName: z.string(),
+});
+
+const getMyself = async () => {
+  const response = await fetch(
+    `https://parklaneit.atlassian.net/rest/api/3/myself`,
+    {
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `devinda.senanayaka@parklane.com.au:${env.JIRA}`
+        ).toString("base64")}`,
+        Accept: "application/json",
+      },
+    }
+  );
+  return MyselfSchema.parseAsync(await response.json());
 };
